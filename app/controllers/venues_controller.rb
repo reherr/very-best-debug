d1241cc07a6e3fac8dd37f615c03b86572f7302e
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-06-17 17:31:37 +0000

    fixed something

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -42,7 +42,7 @@ class VenuesController < ApplicationController
   def destroy
     the_id = params.fetch("venue_id")
     matching_venues = Venue.where({ :id => the_id })
-    venue = matching_venues
+    venue = matching_venues.at(0)
     venue.destroy
 
     redirect_to("/venues")
