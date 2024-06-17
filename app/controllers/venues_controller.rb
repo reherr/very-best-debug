89a15bd8b952a7bf57555a9a48f3a475617472b6
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-06-17 17:29:43 +0000

    fixed update method for venues

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -17,21 +17,22 @@ class VenuesController < ApplicationController
   end
 
   def create
-    @venue = Venue.new
+    venue = Venue.new
     venue.address = params.fetch("query_address")
-    venue.name = params.fetch("name")
-    venue.neighborhood = params.fetch("neighborhood")
+    venue.name = params.fetch("query_name")
+    venue.neighborhood = params.fetch("query_neighborhood")
     venue.save
 
-    redirect_to("/venues/#{venue.name}")
+    redirect_to("/venues/#{venue.id}")
   end
   
   def update
     the_id = params.fetch("venue_id")
 
-    @venue = Venue.where({ :id => the_id })
+    matching_venue = Venue.where({ :id => the_id })
+    venue = matching_venue.at(0)
     venue.address = params.fetch("query_address")
-    venue.name = params.fetch("Query_name")
+    venue.name = params.fetch("query_name")
     venue.neighborhood = params.fetch("query_neighborhood")
     venue.save
     
