fc53ca7ad9d66a1ee73da32598edc7c6c686cf6a
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-06-17 17:09:02 +0000

    fixed venue show detailed page

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -10,7 +10,8 @@ class VenuesController < ApplicationController
   def show
     venue_id = params.fetch("venue_id")
     matching_venues = Venue.where({ :id => venue_id })
-    @the_venue = matching_venues
+
+    @the_venue = matching_venues.at(0)
 
     render({ :template => "venue_templates/details" })
   end
