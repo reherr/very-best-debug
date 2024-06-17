cdcc1447a317f1549382ccfd54d0ccfbc4aa1ce2
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-06-17 17:03:43 +0000

    fixed venue index page

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ Rails.application.routes.draw do
   post("/update_user/:user_id", { :controller => "users", :action => "update" })
 
   get("/venues", { :controller => "venues", :action => "index" })
+  get("/venues/:venue_id", { :controller => "venues", :action => "show" })
   post("/insert_venue_record", { :controller => "venues", :action => "create" })
-  get("/venue/:an_id", { :controller => "venue", :action => "show" })
-  post("/update_venue/:the_id", { :controller => "application", :action => "update" })
-  get("/delete_venue/:id_to_delete", { :controller => "venue", :action => "destroy" })
+  post("/update_venue/:venue_id", { :controller => "venues", :action => "update" })
+  get("/delete_venue/:venue_id", { :controller => "venues", :action => "destroy" })
   
   post("/insert_comment_record", { :controller => "comments", :action => "create" })
 end
